d831686eb99f41edc618c1a368943841b2b97918
Jaewook Kim <103343846+Jaek-Kein@users.noreply.github.com>
2023-10-15 21:09:13 +0900

    w05 task

---
+++ w05/master.js
@@ -0,0 +1,33 @@
+var Body = {
+    setBackgroundColor : function(color){
+        document.querySelector('body').style.backgroundColor = color;
+    },
+    setColor: function(color){
+        document.querySelector('body').style.color = color
+    }
+}
+var Links = {
+    setColor : function(color){
+        var i = 0;
+        var alist = document.querySelectorAll('a');
+        while(i < alist.length){
+        alist[i].style.color = color;
+        i++;
+        }
+    }
+}
+
+function nightDayHandler(self){
+    if(self.value == 'night'){
+        Body.setBackgroundColor('black');
+        Body.setColor('white');
+        Links.setColor('powderblue');
+        self.value='day'
+        }
+    else {
+        Body.setBackgroundColor('white');
+        Body.setColor('black');
+        Links.setColor('blue');
+        self.value='night'
+    }
+}
